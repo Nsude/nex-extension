275f3c62206177d990b3777f44c74f954f8c2710
Meshach Nsude <nsudeme777@gmail.com>
2025-06-23 11:54:44 +0100

    display added profiles implemented

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tabs, type Profile } from "../App";
 import ProfileIcon from "../assets/icons/ProfileIcon";
 import CustomButton from "./CustomButton";
@@ -11,6 +11,15 @@ interface Props {
 
 const MainWrapper = ({selectedTab, profile}: Props) => {
   const [addedProfiles, setAddedProfiles] = useState<Profile[]>([]);
+
+  // load added profiles
+  useEffect(() => {
+    chrome.storage.local.get(['addedProfiles'], (response) => {
+      if (!response.addedProfiles) return;
+
+      setAddedProfiles([...response.addedProfiles]);
+    })
+  }, [])
 
   const handleAddProfile = () => {
     if (!profile) return;
@@ -35,18 +44,18 @@ const MainWrapper = ({selectedTab, profile}: Props) => {
   
   return (
     <div className="w-full pt-[25px]">
-      {/* Tab 1 (Current Profile) */}
-      {
-        selectedTab === tabs[0].label && profile ? (
+      {/* current profile */}
+      {selectedTab === tabs[0].label && (
+        profile ? (
           <div>
             <ProfileCard profile={profile} />
             <span className="absolute bottom-[20px] left-[20px]">
               <CustomButton label="Add Profile" profile={profile} handleClick={handleAddProfile} />
             </span>
           </div>
-
-        ) : selectedTab === tabs[0].label && !profile ? (
-          <div className="flex w-full flex-col gap-y-[10px] opacity-40 mt-15px] items-center text-center">
+        ) : (
+          // empty state
+          <div className="flex w-full flex-col gap-y-[10px] opacity-40 mt-[15px] items-center text-center">
             <span className="w-[65%]">
               To get started, go to a profile then click “Add Profile”
             </span>
@@ -54,25 +63,33 @@ const MainWrapper = ({selectedTab, profile}: Props) => {
               or enable auto-save in “Settings” to add every profile you visit while Nex is running.
             </span>
           </div>
-        ) : null
-      }
+        )
+      )}
 
-      {/* Tab 2 (Added) */}
-      {
-        selectedTab === tabs[1].label && addedProfiles ? (
-          <div>
-
+      {/* added profiles */}
+      {selectedTab === tabs[1].label && (
+        addedProfiles.length > 0 ? (
+          <div className="flex flex-col gap-y-[20px] w-full h-[258px] overflow-y-scroll">
+            {addedProfiles.map((item, i) => (
+              <div className="relative mt-[20px] added-profile-card"
+                key={'profile_' + (i + 1)}>
+                <ProfileCard profile={item} />
+                <span className="h-[1px] w-[95%] absolute bottom-[-20px] left-0 bg-borderGray/50 profile-border" />
+              </div>
+            ))}
           </div>
-        ) : selectedTab === tabs[1].label && !addedProfiles ? (
+        ) : (
+          // empty state
           <div className="flex mt-[50px] justify-center items-center">
             <span className="w-[65%] opacity-40 text-center">
               Seems like you haven’t added anyone yet :)
             </span>
           </div>
-        ) : null
-      }
+        )
+      )}
     </div>
-  )
+  );
+
 }
 
 export default MainWrapper;
