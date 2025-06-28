37cf5651a6bf0870dd5566e9c0e18a525129be7b
Meshach Nsude <nsudeme777@gmail.com>
2025-06-28 14:23:23 +0100

    update autosave path

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss(), 
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/autosave.js',
-          dest: ''
-        }
-      ]
-    })
+    tailwindcss(),
   ],
   build: {
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'index.html'),
-        content: resolve(__dirname, 'src/content.ts')
+        content: resolve(__dirname, 'src/content.ts'),
+        autosave: resolve(__dirname, 'src/autosave.ts')
       },
       output: {
         entryFileNames: assetInfo => {
           if (assetInfo.name === "content") return "content.js";
+          if (assetInfo.name === "autosave") return "autosave.js";
           return "[name].js";
         }
       }
